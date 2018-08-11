7b39b09960d8e283676933e2104e1b25ca618ec7
Mark Tellez <mark.tellez@icloud.com>
2018-08-11 11:11:57 -0500

    basic specimen class

---
+++ src/physics/specimen.js
@@ -0,0 +1,12 @@
+import Entity from './entity'
+
+class Specimen extends Entity {
+  static size = 20
+  static maxSpeed = 500
+
+  constructor(props) {
+    props.size = Specimen.size
+    props.maxSpeed = Specimen.maxSpeed
+    super(props)
+  }
+}
