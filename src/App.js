50d46dff68916a7c81e70e3456a56b58102ed75a
Mark Tellez <mark.tellez@icloud.com>
2018-08-11 09:32:57 -0500

    start-here

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div
+      className="canvas"
+    ></div>
+  }
+}
+
+export default App;
